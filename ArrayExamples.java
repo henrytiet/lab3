54758e129b2a23cc8d64555fef24fd49ccf17862
henrytiet <henrytiet@gmail.com>
2023-01-25 13:32:29 -0800

    Checkpoint 1

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,17 +4,24 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
+    int[] tempArray= new int[arr.length];
+    for(int i=0; i<tempArray.length; i++) {
+      tempArray[i]=arr[i];
+    }
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+      arr[i] = tempArray[tempArray.length - i - 1];
     }
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
   // order
   static int[] reversed(int[] arr) {
-    int[] newArray = new int[arr.length];
+    int[] tempArray = new int[arr.length];
+    for(int i=0; i<tempArray.length; i++) {
+      tempArray[i]=arr[i];
+    }
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      arr[i] = tempArray[arr.length - i - 1];
     }
     return arr;
   }
